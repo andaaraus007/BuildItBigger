95d552ac569d1ab64102c666733830507ea5ccae
eric <and44raus@gmail.com>
2020-09-11 14:05:19 -0700

    clean code

--- JokeSource/src/main/java/com/udacity/jokesource/JokeSource.java
+++ JokeSource/src/main/java/com/udacity/jokesource/JokeSource.java
@@ -1,11 +1,12 @@
 package com.udacity.jokesource;
 
 public class JokeSource {
+    private static final String DEFAULT_JOKE = "Joke? What joke? You're a Joke!";
     public JokeSource() {
 
     }
 
     public String getJoke() {
-        return "Joke? What joke? You're a Joke!";
+        return DEFAULT_JOKE;
     }
 }
