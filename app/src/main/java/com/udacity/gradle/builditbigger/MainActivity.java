95a6c3a92526f775e84b7855cf6fe60967f70ccb
eric <and44raus@gmail.com>
2020-09-12 11:48:47 -0700

    Checking network connectivity first

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,6 +1,9 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -49,6 +52,15 @@ public class MainActivity extends AppCompatActivity implements EndPointsResultIn
     }
 
     public void tellJoke(View view) {
-        new EndPointsAsyncTask(this).execute();
+        if (isNetworkAvailable()) {
+            new EndPointsAsyncTask(this).execute();
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
